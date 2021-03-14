1b9e9ad2d7571cb87575c9c6a159e98c8536f08b
Gustavo Baierski <gustavobaierski@gmail.com>
2021-03-14 11:40:56 -0300

    Criar header e footer

---
+++ app/view/footer.php
@@ -0,0 +1,8 @@
+<footer class="rodape">
+	<div class="info-rodape">
+		<p>Brazuca Craft - Todos os direitos reservados</p>
+		<p>Equipe BrazucaCraft</p>
+		<p>2021</p>
+
+	</div>
+</footer>
---
+++ app/view/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta charset="utf-8">
+
+		<link rel="stylesheet" type="text/css" href="../../assets/css/style.css"/>
+		<link rel="shortcut icon" href="../../assets/imagens/icon_mercado.ico" />
+    </head>
+
+    <body>
+
+        <header class="header" style="position: fixed;">
+
+        </header>
