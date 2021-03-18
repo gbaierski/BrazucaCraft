3d7decca8adf19695db12e979adb8dc847332c7d
Gustavo Baierski <gustavobaierski@gmail.com>
2021-03-18 01:00:33 -0300

    Criar o layout inicial da home

--- app/view/header.php
+++ app/view/header.php
@@ -1,16 +1,29 @@
 <!DOCTYPE html>
 	<html lang="zxx" class="no-js">
-
+	
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="../../assets/css/home.css"/>
 
-		<link rel="stylesheet" type="text/css" href="../../assets/css/style.css"/>
-		<link rel="shortcut icon" href="../../assets/imagens/icon_mercado.ico" />
     </head>
 
     <body>
 
-        <header class="header" style="position: fixed;">
+        <header class="header">
+			<nav class="nav-menu">
 
+				<a href="">
+				<p class="header-texto" id="texto-brazuca"> BRAZUCA </p>
+				<p class="header-texto" id="texto-craft"> CRAFT </p>
+				</a>
+
+				<div class="opcao-menu"><button class="botao-menu" id="botao-home"><a href="" class="link-menu" >HOME</a></button></div>
+				<div class="opcao-menu"><button class="botao-menu" id="botao-loja"><a href="" class="link-menu" >LOJA</a></button></div>
+				<div class="opcao-menu"><button class="botao-menu" id="botao-forum"><a href="" class="link-menu" >FÓRUM</a></button></div>
+				<div class="opcao-menu"><button class="botao-menu" id="botao-sobre"><a href="" class="link-menu" >SOBRE</a></button></div>
+				<div class="opcao-menu"><button class="botao-menu" id="botao-ajuda"><a href="" class="link-menu" >AJUDA</a></button></div>
+
+				<div id="head-player"></div>
+			</nav>
         </header>
--- app/view/home.php
+++ app/view/home.php
@@ -1,13 +1,14 @@
 <?php require 'header.php' ?>
 
 <div class="logo"></div>
-<div class="background_home"></div>
+<div class="background-home"></div>
+<div id="ip-home"> mc.brazucacraft.net </div>
 
 <div class="clear"></div>
 
 <div class="info-section">
 	<div class="informacao-home">
-		Brazuca
+		
 	</div>
 
 </div>
