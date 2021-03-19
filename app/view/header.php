b7237113d06cdeb0439f360290f727cc44b5194c
Gustavo Baierski <gustavobaierski@gmail.com>
2021-03-18 23:34:56 -0300

    Mudar organização do menu e criar novo arquivo css

--- app/view/header.php
+++ app/view/header.php
@@ -4,8 +4,11 @@
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="../../assets/css/home.css"/>
 
+		<link rel="stylesheet" type="text/css" href="../../assets/css/geral.css"/>
+		<link rel="stylesheet" type="text/css" href="../../assets/css/header.css"/>
+		<link rel="stylesheet" type="text/css" href="../../assets/css/home.css"/>
+		
     </head>
 
     <body>
@@ -13,17 +16,16 @@
         <header class="header">
 			<nav class="nav-menu">
 
-				<a href="">
-				<p class="header-texto" id="texto-brazuca"> BRAZUCA </p>
-				<p class="header-texto" id="texto-craft"> CRAFT </p>
+				<a href="" id="link-brazuca-menu">
+				<p class="header-texto" id="texto-brazuca"> BRAZUCA <br> CRAFT</p>
 				</a>
 
-				<div class="opcao-menu"><button class="botao-menu" id="botao-home"><a href="" class="link-menu" >HOME</a></button></div>
-				<div class="opcao-menu"><button class="botao-menu" id="botao-loja"><a href="" class="link-menu" >LOJA</a></button></div>
-				<div class="opcao-menu"><button class="botao-menu" id="botao-forum"><a href="" class="link-menu" >FÓRUM</a></button></div>
-				<div class="opcao-menu"><button class="botao-menu" id="botao-sobre"><a href="" class="link-menu" >SOBRE</a></button></div>
-				<div class="opcao-menu"><button class="botao-menu" id="botao-ajuda"><a href="" class="link-menu" >AJUDA</a></button></div>
-
+				<div class="opcao-menu"><button class="botao-menu" id="botao-home"><a href="" class="link-menu" ><img src="../../assets/img/icons/home.png" class="icon-linkMenu">HOME</a></button></div>
+				<div class="opcao-menu"><button class="botao-menu" id="botao-forum"><a href="" class="link-menu" ><img src="../../assets/img/icons/forum.png" class="icon-linkMenu">FÓRUM</a></button></div>
+				<div class="opcao-menu"><button class="botao-menu" id="botao-sobre"><a href="" class="link-menu" ><img src="../../assets/img/icons/sobre.png" class="icon-linkMenu">SOBRE</a></button></div>
+				<div class="opcao-menu"><button class="botao-menu" id="botao-ajuda"><a href="" class="link-menu" ><img src="../../assets/img/icons/ajuda.png" class="icon-linkMenu">AJUDA</a></button></div>
+				
+				<div id="div-loja"><button class="botao-menu" id="botao-loja"><a href="" class="link-menu" ><img src="../../assets/img/icons/loja.png" class="icon-linkMenu">LOJA</a></button></div>
 				<div id="head-player"></div>
 			</nav>
         </header>
