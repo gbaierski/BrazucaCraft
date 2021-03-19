d27519f2d2901ffe53a394641651ad97c497a5d1
Gustavo Baierski <gustavobaierski@gmail.com>
2021-03-19 00:34:33 -0300

    Melhorar a visibilidade do ip, adicionar numero de jogadores, corrigir links do menu

--- app/view/header.php
+++ app/view/header.php
@@ -20,12 +20,12 @@
 				<p class="header-texto" id="texto-brazuca"> BRAZUCA <br> CRAFT</p>
 				</a>
 
-				<div class="opcao-menu"><button class="botao-menu" id="botao-home"><a href="" class="link-menu" ><img src="../../assets/img/icons/home.png" class="icon-linkMenu">HOME</a></button></div>
-				<div class="opcao-menu"><button class="botao-menu" id="botao-forum"><a href="" class="link-menu" ><img src="../../assets/img/icons/forum.png" class="icon-linkMenu">FÓRUM</a></button></div>
-				<div class="opcao-menu"><button class="botao-menu" id="botao-sobre"><a href="" class="link-menu" ><img src="../../assets/img/icons/sobre.png" class="icon-linkMenu">SOBRE</a></button></div>
-				<div class="opcao-menu"><button class="botao-menu" id="botao-ajuda"><a href="" class="link-menu" ><img src="../../assets/img/icons/ajuda.png" class="icon-linkMenu">AJUDA</a></button></div>
+				<a href=""><div class="opcao-menu"><button class="botao-menu" id="botao-home"><img src="../../assets/img/icons/home.png" class="icon-linkMenu">HOME</button></a></div>
+				<div class="opcao-menu"><a href=""><button class="botao-menu" id="botao-forum"><img src="../../assets/img/icons/forum.png" class="icon-linkMenu">FÓRUM</button></a></div>
+				<div class="opcao-menu"><a href=""><button class="botao-menu" id="botao-sobre"><img src="../../assets/img/icons/sobre.png" class="icon-linkMenu">SOBRE</button></a></div>
+				<div class="opcao-menu"><a href=""><button class="botao-menu" id="botao-ajuda"><img src="../../assets/img/icons/ajuda.png" class="icon-linkMenu">AJUDA</button></a></div>
 				
-				<div id="div-loja"><button class="botao-menu" id="botao-loja"><a href="" class="link-menu" ><img src="../../assets/img/icons/loja.png" class="icon-linkMenu">LOJA</a></button></div>
+				<div id="div-loja"><a href="" ><button class="botao-menu" id="botao-loja"><img src="../../assets/img/icons/loja.png" class="icon-linkMenu">LOJA</button></a></div>
 				<div id="head-player"></div>
 			</nav>
         </header>
--- app/view/home.php
+++ app/view/home.php
@@ -1,11 +1,15 @@
 <?php require 'header.php' ?>
 
-<div class="logo"></div>
+<div id="logo"></div>
 <div class="background-home"></div>
-<div id="ip-home"> mc.brazucacraft.net </div>
+<div id="botao-ip">
+	<div id="ip-home"> mc.brazucacraft.net </div>
+</div>
 
 <div class="clear"></div>
 
+<div id="div-players">5297<img src="../../assets/img/icons/players.png" id="icon-players"></div>
+<div id="div-jogadoresOnline">JOGADORES ONLINE</div>
 <div class="info-section">
 	<div class="informacao-home">
 		
@@ -14,5 +18,5 @@
 </div>
 
 <div class="clear"></div>
-
+<br>
 <?php require 'footer.php'; ?>
