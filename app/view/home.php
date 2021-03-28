dc9f7d6e767deadc7c3ecf944f02e30c33394d6f
Gustavo Baierski <gustavobaierski@gmail.com>
2021-03-28 00:51:53 -0300

    Adicionar a primeira parte do conteúdo da Home (Link do Discord)

--- app/view/home.php
+++ app/view/home.php
@@ -10,16 +10,16 @@
 
 <div id="div-players">5297<img src="../../assets/img/icons/players.png" id="icon-players"></div>
 <div id="div-jogadoresOnline">JOGADORES ONLINE</div>
-<div class="info-section">
-	<div class="informacao-home" style="float: right">
-		<a href="https://discord.com/" ><img src="../../assets/img/icons/discord.png" class="redeSocial-home"></a>
-		<a href="https://www.facebook.com/" ><img src="../../assets/img/icons/facebook.png" class="redeSocial-home"></a>
-		<a href="https://twitter.com/" ><img src="../../assets/img/icons/twitter.png" class="redeSocial-home"></a>
-		<a href="https://www.instagram.com/" ><img src="../../assets/img/icons/instagram.png" class="redeSocial-home"></a>
-	</div>
-
+<div class="informacao-home" style="float: right">
+	<a href="https://discord.com/" ><img src="../../assets/img/icons/discord.png" class="redeSocial-home"></a>
+	<a href="https://www.facebook.com/" ><img src="../../assets/img/icons/facebook.png" class="redeSocial-home"></a>
+	<a href="https://twitter.com/" ><img src="../../assets/img/icons/twitter.png" class="redeSocial-home"></a>
+	<a href="https://www.instagram.com/" ><img src="../../assets/img/icons/instagram.png" class="redeSocial-home"></a>
 </div>
-
 <div class="clear"></div>
-<br>
+<div id="div-discord">
+	<a href="https://discord.com/"><div id="imagem-discord"></div></a>
+	<button id="frase-discord"><a href="https://discord.com/" id="link-discord">JÁ CONHECE NOSSO DISCORD!?</a></button>
+	<iframe id="iframe-discord" src="https://discord.com/widget?id=704695441677221898&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+</div>
 <?php require 'footer.php'; ?>
