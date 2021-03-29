1deb4c16330ee032838ec8a0ab257013fe3f0396
Soethe <soethe42@gmail.com>
2021-03-28 23:30:21 -0300

    Corrigir echo da loja.php

--- app/view/loja.php
+++ app/view/loja.php
@@ -13,24 +13,27 @@
 <!-- Div da compra -->
 <div class="row text-center">
 <div class="col-lg-3 col-md-6 mb-4">
-<?php foreach ($produtos as $produto): ?>
+<?php foreach ($produtos as $produto){
 
-    <div class="card h-100">
-      <img class="card-img-top" src="../../assets/img/vips/<?= $produto['imagemProduto']?>" alt="">
+      echo '<div class="card h-100">
+      <img class="card-img-top" src="../../assets/img/vips/'.$produto['imagemProduto'].' " alt="">
       <div class="card-body">
-        <h4 class="card-title"><?= $produto['nomeProduto']?></h4>
-        <p class="card-text"> <?= $produto['precoProduto']?> </p>
-        <p class="card-text"> <?= $produto['descricaoProduto']?> </p>
+        <h4 class="card-title"> '.$produto['nomeProduto'].'</h4>
+        <p class="card-text">  '.$produto['precoProduto'].' </p>
+        <p class="card-text">  '.$produto['descricaoProduto'] .' </p>
       </div>
       <div class="card-footer">
         <a href="#" class="btn btn-success">Comprar</a>
-        <? // Colocar id de SESSION para administrador ?> <a href="#" class="btn btn-danger">Editar</a>
+     <a href="#" class="btn btn-danger">Editar</a>
       </div>
     </div>
-  </div>
+  </div>';
+} 
+ ?>
+
+    
 
 
-<?php endforeach ?>
 </div> 
 </div>
 
