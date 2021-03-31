5cd4f0061237cb4016340858aa0d6d2b9f65a2e3
Soethe <soethe42@gmail.com>
2021-03-31 18:28:05 -0300

    Resolver erro da prencher em branco no banco , deixar descrição para depois

--- app/view/cadastroProdutos.php
+++ app/view/cadastroProdutos.php
@@ -6,26 +6,30 @@
         <h2 id="cadastroProduto-titulo"><?= $nomeForm ?></h2>
            
 
-            <?php if($edicao == true){
+            <?php
+                // Parte Edição      
+            if($edicao == true){
 
                 foreach ($produtos as $produto) { ?>
 
                 <form action="..\control\produtos.php?action=editarProduto&ai_produto=<?=$produto['ai_produto']?>" method="post" id="form-cadastroProdutos">
 
                 <label for="nomeproduto" class="label-cadastroProduto">Nome do Produto:</label>
-                <input placeholder="<?= $produto['nomeProduto'] ?> " type="text" name="nomeProduto"  class="cadastroProduto" id="nomeProduto">
+                <input value="<?= $produto['nomeProduto'] ?>" placeholder="<?= $produto['nomeProduto'] ?> " type="text" name="nomeProduto"  class="cadastroProduto" id="nomeProduto">
 
                 <label for="precoProduto" class="label-cadastroProduto">Preço:</label>
-                <input placeholder="<?= $produto['precoProduto'] ?>"  type="text" name="precoProduto" class="cadastroProduto" id="precoProduto">
+                <input value="<?= $produto['precoProduto'] ?>" placeholder="<?= $produto['precoProduto'] ?>"  type="text" name="precoProduto" class="cadastroProduto" id="precoProduto">
 
                 <label   for="descricaoProduto" class="label-cadastroProduto" id="label-descricaoProduto">Descrição:</label>
-                <textarea placeholder="<?= $produto['descricaoProduto'] ?> "  name="descricaoProduto" class="cadastroProduto" id="descricaoProduto" rows="5" ></textarea>
+                <textarea type="text" value="<?= $produto['descricaoProduto'] ?>" placeholder="<?= $produto['descricaoProduto'] ?> "  name="descricaoProduto" class="cadastroProduto" id="descricaoProduto" rows="5" ></textarea>
 
                 <div class="clear"></div>
 
                 <input type="submit" name="cadastrarProduto" value="Editar" id="botao-cadastrarProduto">
-            <?php };
-                }else{ ?>
+            <?php }
+                }else{ 
+                    // Parte Cadastro
+                    ?>
 
                 <form action="..\control\produtos.php?action=cadastroProduto" method="post" id="form-cadastroProdutos">
 
