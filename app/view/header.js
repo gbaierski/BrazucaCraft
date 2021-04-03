dad6b1185f5166862e28d877262e25ab7d42ecec
Gustavo Baierski <gustavobaierski@gmail.com>
2021-04-03 18:32:35 -0300

    Início da responsividade do site (Mobile/Tablet)

--- app/view/header.js
+++ app/view/header.js
@@ -1,10 +1,8 @@
 var modal = document.getElementById("modal");
-
-var btn = document.getElementById("botao-login");
-
+var botaoLogin = document.getElementById("botao-login");
 var span = document.getElementsByClassName("fechar")[0];
 
-btn.onclick = function() {
+botaoLogin.onclick = function() {
 
 modal.style.display = "block";
 }
@@ -14,9 +12,27 @@ span.onclick = function() {
 modal.style.display = "none";
 }
 
-window.onclick = function(event) {
-    
-if (event.target == modal) {
-    modal.style.display = "none";
+function abrirDropdown() {
+
+    document.getElementById("myDropdown").classList.toggle("show");
 }
+
+window.onclick = function(event) {
+
+    //Dropdown
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+
+            openDropdown.classList.remove('show');
+          }
+        }
+    }
+
+    if (event.target == modal) {
+    modal.style.display = "none";
+    }
 }
--- app/view/header.php
+++ app/view/header.php
@@ -8,7 +8,7 @@
 	<head>
 		<title> <?= $nomePagina ?> </title>
 		
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta charset="utf-8">
 
 		<!-- Css de páginas main -->
@@ -28,7 +28,7 @@
 
     <body>
         <header class="header">
-          <nav class="nav-menu">
+          <nav class="nav-desktop">
 
             <a href="../control/redirecionamento.php?action=index" id="link-brazuca-menu">
               <p class="header-texto" id="texto-brazuca"> BRAZUCA <br> CRAFT</p>
@@ -38,7 +38,7 @@
               <a href="../control/redirecionamento.php?action=index">
                 <button class="botao-menu" id="botao-home">
                   <img src="../../assets/img/icons/home.png" class="icon-linkMenu">
-                  HOME
+                  <div class="texto-linkMenu">HOME</div>
                 </button>
               </a>
             </div>
@@ -47,7 +47,7 @@
               <a href="">
                 <button class="botao-menu" id="botao-blog">
                   <img src="../../assets/img/icons/blog.png" class="icon-linkMenu">
-                  BLOG
+                  <div class="texto-linkMenu"> BLOG </div>
                 </button>
               </a>
             </div>
@@ -56,7 +56,7 @@
               <a href="../control/redirecionamento.php?action=paginacadastroprodutos">
                 <button class="botao-menu" id="botao-sobre">
                   <img src="../../assets/img/icons/sobre.png" class="icon-linkMenu">
-                  SOBRE
+                  <div class="texto-linkMenu"> SOBRE </div>
                 </button>
               </a>
             </div>
@@ -65,7 +65,7 @@
               <a href="">
                 <button class="botao-menu" id="botao-ajuda">
                   <img src="../../assets/img/icons/ajuda.png" class="icon-linkMenu">
-                  AJUDA
+                  <div class="texto-linkMenu"> AJUDA </div>
                 </button>
               </a>
             </div>
@@ -74,13 +74,73 @@
               <a href="../control/redirecionamento.php?action=paginaLoja" >
                 <button class="botao-menu" id="botao-loja">
                   <img src="../../assets/img/icons/loja.png" class="icon-linkMenu">
-                  LOJA
+                  <div class="texto-linkMenu"> LOJA </div>
                 </button>
               </a>
             </div>
                                           <!-- id="head-player" -->
             <?= isset($_SESSION['login']) ?'<div  class="dropdown">
-                                              <button onclick="myFunction()" id="head-player" class="dropbtn"></button>
+                                              <button onclick="abrirDropdown()" id="head-player" class="dropbtn"></button>
+                                              <div id="myDropdown" class="dropdown-content">
+                                                <a href="#">Link 1</a>
+                                                <a href="#">Link 2</a>
+                                                <a href="../control/usuario.php?action=deslogar">Deslogar</a>
+                                              </div>
+                                            </div>' : '<button id="botao-login">LOGIN</button>'; ?>
+          </nav>
+          <nav class="nav-mobile">
+
+            <div class="opcao-menuMobile">
+              <a href="../control/redirecionamento.php?action=index">
+                <button class="botao-menuMobile" id="botao-homeMobile">
+                  <img src="../../assets/img/icons/home.png" class="icon-linkMenuMobile">
+                  <div class="texto-linkMenuMobile">HOME</div>
+                </button>
+              </a>
+            </div>
+
+            <a href="../control/redirecionamento.php?action=index" id="link-brazuca-menu">
+              <p class="header-texto" id="texto-brazuca"> BRAZUCA <br> CRAFT</p>
+            </a>
+
+            <div class="opcao-menuMobile">
+              <a href="">
+                <button class="botao-menuMobile" id="botao-blogMobile">
+                  <img src="../../assets/img/icons/blog.png" class="icon-linkMenuMobile">
+                  <div class="texto-linkMenuMobile"> BLOG </div>
+                </button>
+              </a>
+            </div>
+
+            <div class="opcao-menuMobile">
+              <a href="../control/redirecionamento.php?action=paginacadastroprodutos">
+                <button class="botao-menuMobile" id="botao-sobreMobile">
+                  <img src="../../assets/img/icons/sobre.png" class="icon-linkMenuMobile">
+                  <div class="texto-linkMenuMobile"> SOBRE </div>
+                </button>
+              </a>
+            </div>
+
+            <div class="opcao-menuMobile">
+              <a href="">
+                <button class="botao-menuMobile" id="botao-ajudaMobile">
+                  <img src="../../assets/img/icons/ajuda.png" class="icon-linkMenuMobile">
+                  <div class="texto-linkMenuMobile"> AJUDA </div>
+                </button>
+              </a>
+            </div>
+
+            <div id="div-loja">
+              <a href="../control/redirecionamento.php?action=paginaLoja" >
+                <button class="botao-menuMobile" id="botao-lojaMobile">
+                  <img src="../../assets/img/icons/loja.png" class="icon-linkMenuMobile">
+                  <div class="texto-linkMenuMobile"> LOJA </div>
+                </button>
+              </a>
+            </div>
+                                          <!-- id="head-player" -->
+            <?= isset($_SESSION['login']) ?'<div  class="dropdown">
+                                              <button onclick="abrirDropdown()" id="head-player" class="dropbtn"></button>
                                               <div id="myDropdown" class="dropdown-content">
                                                 <a href="#">Link 1</a>
                                                 <a href="#">Link 2</a>
@@ -130,44 +190,7 @@
             </div>
             
 <script>
-// Funções Dropdown
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
 
-
-// Funçoes Modal
-var modal = document.getElementById("modal");
-
-var btn = document.getElementById("botao-login");
-
-var span = document.getElementsByClassName("fechar")[0];
-
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
-
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-
-  window.onclick = function(event) {
-    if (event.target == modal) {
-     modal.style.display = "none";
-   }
-  }
   
 </script>
 
