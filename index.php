5234ac449123fe9a2f7e348b9b796b2bd9905764
Gustavo Baierski <gustavobaierski@gmail.com>
2021-04-18 20:15:54 -0300

    Criar tela provisória para autenticação de membros da equipe durante a fase de testes

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
-header("location: app/control/redirecionamento.php?action=index");
-
+//REMOVER
+//header("location: app/control/redirecionamento.php?action=index");
+header("location: app/view/autenticacaoProvisoria.php");
 ?>
