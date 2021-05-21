90bcd26bc2fd13b86dfacd5f48b197f92716f82d
Gustavo Baierski <gustavobaierski@gmail.com>
2021-05-21 00:53:55 -0300

    Criar o conteúdo da seção de moedas

--- app/view/loja/paginaMoedas.php
+++ app/view/loja/paginaMoedas.php
@@ -7,6 +7,30 @@
 
     <div id="tituloMoedas"> Moedas</div>
 
+    <div class="produtoMoeda">
+        <img class="imagemMoeda" src="../../assets/img/paginas/home/backgroundHome.png">
+        <div class="quantidadeMoeda">500 MOEDAS</div>
+        <button class="botaoComprarMoeda">R$ 5,00</button>
+    </div>
+
+    <div class="produtoMoeda">
+        <img class="imagemMoeda" src="../../assets/img/paginas/home/backgroundHome.png">
+        <div class="quantidadeMoeda">1000 MOEDAS</div>
+        <button class="botaoComprarMoeda">R$ 10,00</button>
+    </div>
+
+    <div class="produtoMoeda">
+        <img class="imagemMoeda" src="../../assets/img/paginas/home/backgroundHome.png">
+        <div class="quantidadeMoeda">5000 MOEDAS</div>
+        <button class="botaoComprarMoeda">R$ 50,00</button>
+    </div>
+
+    <div class="produtoMoeda">
+        <img class="imagemMoeda" src="../../assets/img/paginas/home/backgroundHome.png">
+        <div class="quantidadeMoeda">10000 MOEDAS</div>
+        <button class="botaoComprarMoeda">R$ 100,00</button>
+    </div>
+
 </section>
 
 <div class="clear"></div>
