7783379a8dc666cb32244be2b3f97ba29d352966
Gustavo Baierski <gustavobaierski@gmail.com>
2021-05-21 00:29:39 -0300

    Criar conteúdo da seção de vips e corrigir cores dos botões da loja

--- app/view/loja/paginaVips.php
+++ app/view/loja/paginaVips.php
@@ -7,22 +7,75 @@
     </a>
 
     <div id="tituloVips"> Vips BrazucaCraft</div>
+
     <div class="colunaVip">
-        <div></div>
+        <div class="imagemVip" style="background-color: rebeccapurple;"></div>
+        <div class="tituloProdutoVip">VIP PODRE</div>
+        <div class="conteudoVip">
+            <?php
+            $beneficiosVip = ['Terreno rank B','R$ 1000,00', '3 Caixas Misteriosas', '2 Cosméticos', '3 emojis'];
+
+            foreach ($beneficiosVip as $beneficio) {
+
+                echo $beneficio;
+                echo '</br>';
+            }
+            ?>
+        </div>
+        <button class="botaoComprarVip">R$ 10,00</button>
     </div>
 
     <div class="colunaVip">
-        <div></div>
+        <div class="imagemVip" style="background-color: red;"></div>
+        <div class="tituloProdutoVip">VIP BÁSICO</div>
+        <div class="conteudoVip">
+            <?php
+            $beneficiosVip = ['Terreno rank A','R$ 2000,00', '4 Caixas Misteriosas', '2 Cosméticos', '3 emojis'];
+
+            foreach ($beneficiosVip as $beneficio) {
+
+                echo $beneficio;
+                echo '</br>';
+            }
+            ?>
+        </div>
+        <button class="botaoComprarVip">R$ 20,00</button>
     </div>
 
     <div class="colunaVip">
-        <div></div>
+        <div class="imagemVip" style="background-color: green;"></div>
+        <div class="tituloProdutoVip">VIP MÉDIO</div>
+        <div class="conteudoVip">
+            <?php
+            $beneficiosVip = ['Terreno rank A+','R$ 3000,00', '10 Caixas Misteriosas', '7 Cosméticos', '3 emojis'];
+
+            foreach ($beneficiosVip as $beneficio) {
+
+                echo $beneficio;
+                echo '</br>';
+            }
+            ?>
+        </div>
+        <button class="botaoComprarVip">R$ 40,00</button>
     </div>
 
     <div class="colunaVip">
-        <div></div>
-    </div>
+        <div class="imagemVip" style="background-color: blue;"></div>
+        <div class="tituloProdutoVip">VIP BRABO</div>
+        <div class="conteudoVip">
+            <?php
+            $beneficiosVip = ['Terreno rank S','R$ 10000,00', '15 Caixas Misteriosas', '10 Cosméticos', '7 emojis'];
 
+            foreach ($beneficiosVip as $beneficio) {
+
+                echo $beneficio;
+                echo '</br>';
+            }
+            ?>
+        </div>
+        <button class="botaoComprarVip">R$ 100,00</button>
+    </div>
+    
 </section>
 
 <div class="clear"></div>
