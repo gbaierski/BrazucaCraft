147554f216ac20f108f0abe4509ec5558926284c
Gustavo Baierski <gustavobaierski@gmail.com>
2021-05-22 15:30:25 -0300

    Criar conteúdo da seção de Unban

--- app/view/loja/paginaUnban.php
+++ app/view/loja/paginaUnban.php
@@ -7,6 +7,14 @@
 
     <div id="tituloUnban"> Unban</div>
 
+    <button id="botaoUnban">Como funciona o UnBan?</button>
+
+    <div class="produtoUnban">
+        <img class="imagemUnban" src="../../assets/img/paginas/home/logo.png">
+        <div class="tituloUnban">Unban 1</div>
+        <div class="descricaoUnban">Retira seu banimento permanentemente</div>
+        <button class="botaoComprarUnban">R$ 30,00</button>
+    </div>
 </section>
 
 <div class="clear"></div>
